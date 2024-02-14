15506177351913faa2281d341034fca6e048bbed
Haniya Ahmed <haniya.ahmed@ucalgary.ca>
2024-02-13 22:45:54 -0700

    page4.html and css updated

--- UI/app.js
+++ UI/app.js
@@ -1,14 +1,22 @@
-import Question from './logic/question.js';
-import Quiz from './logic/quiz.js';
-import User from './logic/user.js';
+import Question from "./logic/question.js";
+import Quiz from "./logic/quiz.js";
+import User from "./logic/user.js";
 
-const question1 = new Question("What is the capital of France?", ["Paris", "Germany", "Berlin"], "Paris");
+const question1 = new Question(
+  "What is the capital of France?",
+  ["Paris", "Germany", "Berlin"],
+  "Paris"
+);
 console.log(question1.text); // Output: "What is the capital of France?"
 
-const question2 = new Question("What is the largest mammal?", ["Elephant", "Blue whale", "Rabbit"], "Blue whale");
+const question2 = new Question(
+  "What is the largest mammal?",
+  ["Elephant", "Blue whale", "Rabbit"],
+  "Blue whale"
+);
 console.log(question2.text); // Output: "What is the largest mammal?"
 
-const quiz = new Quiz([question1, question2]); 
+const quiz = new Quiz([question1, question2]);
 const user = new User("Asma");
 
 /* Asynchronous Data Handling
@@ -18,32 +26,29 @@ const user = new User("Asma");
 */
 
 async function fetchData() {
-    const response = await fetch("https://opentdb.com/api.php?amount=50&category=27&type=multiple");
-    const data = await response.json();
-    console.log(data);
-    return data;
+  const response = await fetch(
+    "https://opentdb.com/api.php?amount=50&category=27&type=multiple"
+  );
+  const data = await response.json();
+  console.log(data);
+  return data;
 }
 
-async function processData(){
-    try {
+async function processData() {
+  try {
+    console.log("THIS STARTS");
 
-        console.log("THIS STARTS");
+    //Store JSON Object
+    const questionData = await fetchData();
 
-        //Store JSON Object
-        const questionData = await fetchData();
+    console.log(questionData.results[0].question);
 
-        console.log(questionData.results[0].question);
+    console.log("THIS DONE");
 
-        console.log("THIS DONE");
-
-        document.getElementById("actual-question").innerHTML = questionData.results[0].question;
-        console.log(questionData.results[0].question);
-        
-
-    } catch (error) {
-        
-    }
+    document.getElementById("actual-question").innerHTML =
+      questionData.results[0].question;
+    console.log(questionData.results[0].question);
+  } catch (error) {}
 }
 
 processData();
-
